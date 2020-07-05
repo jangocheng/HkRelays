4f6287497c67db9e1ec35351ef137c6649ddb157
huex <huexpub@gmail.com>
2020-07-05 14:33:38 +0800

    更新烧写波特率

--- Flash_tools/firmware.bat
+++ Flash_tools/firmware.bat
@@ -1,2 +1,2 @@
-bin\esptool.exe  --chip esp32  --before default_reset  --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0xe000 firmware\boot_app0.bin 0x1000 firmware\bootloader_dio_40m.bin 0x10000 firmware\firmware.bin 0x8000 firmware\partitions.bin 
+bin\esptool.exe  --chip esp32   --baud 921600  --before default_reset  --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0xe000 firmware\boot_app0.bin 0x1000 firmware\bootloader_dio_40m.bin 0x10000 firmware\firmware.bin 0x8000 firmware\partitions.bin 
 pause
